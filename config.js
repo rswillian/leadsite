f573ddf6b64ab1e5344834ebafb423d347f7018d
rswillian <willianspieckert31@gmail.com>
2025-08-29 17:48:19 -0300

    changes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwL-TotVwyPNb1fMmhvRtrr9wfYBgxa98dpGyzYTj1_3AgDZmuZbbm0bD1XegMRF4R3/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyIvBDoPJnRK1yPXvEeYT7K0My5dkhAhvCDu5SA61Z7sZrNfqLTJjAtEo4YQl40lco3/exec"
 };
